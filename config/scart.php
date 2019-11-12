e601d340e8611d3cc76c0084f3dea0a8cfe7524a
Naruto <lanhktc@gmail.com>
2019-11-12 22:49:35 +0700

    no message

--- config/scart.php
+++ config/scart.php
@@ -1,6 +1,6 @@
 <?php
 return [
-    'version' => '3.2-beta',
+    'version' => '3.2.0',
     'homepage' => 'https://s-cart.org',
     'name' => 'S-Cart',
     'title' => 'Free Open Source eCommerce for Business',
