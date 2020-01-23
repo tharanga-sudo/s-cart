d5de7dba5a98def417ba5f816657c2d09b728492
Naruto <lanhktc@gmail.com>
2020-01-24 05:48:04 +0700

    no message

--- config/scart.php
+++ config/scart.php
@@ -1,11 +1,13 @@
 <?php
 return [
-    'version' => '3.4.3',
+    'version' => '4.0-beta',
     'homepage' => 'https://s-cart.org',
     'name' => 'S-Cart',
     'title' => 'Free Open Source eCommerce for Business',
     'github' => 'https://github.com/lanhktc/s-cart',
     'email' => 'lanhktc@gmail.com',
     'settings' => [
+        'api_plugin' => 1,
+        'api_template' => 1,
     ],
 ];
