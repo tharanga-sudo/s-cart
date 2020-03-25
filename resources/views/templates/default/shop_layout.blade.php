2540a91c47994d8cfcf30b2b78a7288144f89b14
Core <nteeje@gmail.om>
2020-03-25 22:03:13 +0530

    GAnalytics Implement

--- resources/views/templates/default/shop_layout.blade.php
+++ resources/views/templates/default/shop_layout.blade.php
@@ -8,6 +8,17 @@
         <!DOCTYPE html>
 <html lang="{{ app()->getLocale() }}">
 <head>
+
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-2CG1T5QC5S"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-2CG1T5QC5S');
+    </script>
+
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="{{ $description??sc_store('description') }}">
